ec59cacd4d14bdecf0f9ca886ae31c9f8c4209fc
Aditya Mysore <ammysore@amazon.com>
2025-10-22 13:51:02 -0400

    Removed cross account test

--- .project_automation/functional_tests/functional_tests.sh
+++ .project_automation/functional_tests/functional_tests.sh
@@ -13,7 +13,7 @@ cd ${PROJECT_PATH}
 # Look up the mandatory test files
 EXAMPLE_1_TEST_PATH="./tests/01_efs_to_s3.tftest.hcl"
 EXAMPLE_2_TEST_PATH="./tests/02_s3_to_s3.tftest.hcl"
-EXAMPLE_3_TEST_PATH="./tests/03_s3_to_s3_xaccount.tftest.hcl"
+#EXAMPLE_3_TEST_PATH="./tests/03_s3_to_s3_xaccount.tftest.hcl"
 
 if test -f ${EXAMPLE_1_TEST_PATH}; then
     echo "File ${EXAMPLE_1_TEST_PATH} is found, resuming test"
@@ -29,12 +29,12 @@ else
     (exit 1)
 fi 
 
-if test -f ${EXAMPLE_3_TEST_PATH}; then
-    echo "File ${EXAMPLE_3_TEST_PATH} is found, resuming test"
-else
-    echo "File ${EXAMPLE_3_TEST_PATH} not found. You must include at least one test run in file ${EXAMPLE_3_TEST_PATH}"
-    (exit 1)
-fi 
+# if test -f ${EXAMPLE_3_TEST_PATH}; then
+#     echo "File ${EXAMPLE_3_TEST_PATH} is found, resuming test"
+# else
+#     echo "File ${EXAMPLE_3_TEST_PATH} not found. You must include at least one test run in file ${EXAMPLE_3_TEST_PATH}"
+#     (exit 1)
+# fi 
 
 # Run Terraform test
 terraform init
